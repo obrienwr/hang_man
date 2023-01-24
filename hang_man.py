c62f74b26cd31464c6d69645bf5102cf5f334323
williebees <66076410+williebees@users.noreply.github.com>
2023-01-24 16:48:23 -0500

    added some method headers

---
+++ hang_man.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class HangMan:
+
+def __init__(num_words: Optional[int] = -1):
+	pass
+
+def get_phrase() -> str:
+	pass
+
+def find_idx_of(phrase, letter) -> List[str]:
+	pass
+
+def draw_man():
+	pass
+
+def give_hint():
+	pass
+
+def guess_phrase(guess: str) -> bool:
+	pass
+
+def add_to_graveyard(letter):
+	pass
+
+def win():
+	pass
