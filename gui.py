985b654baed14fbb59cd5c62e6b152d5672131f3
Tayo Olofintuyi <tolofi152@gmail.com>
2023-02-07 18:02:20 -0500

    trying to make the window update with input, left comment on line 29 og gui.py

--- gui.py
+++ gui.py
@@ -8,23 +8,26 @@ class gui:
         self.pictures = ['./pic.png']
         self.man = HangMan()
         self.graveyard = self.man._graveyard
-        self.blanks = self.man.get_phrase()
-        self.layout = [ [ [sg.Image(self.pictures[0])], [sg.Text(self.blanks)]], [sg.Text(self.graveyard), sg.InputText()],
+        self.layout = [ [ [sg.Image(self.pictures[0])], [sg.Text("Phrase: " + ' '.join(self.man.get_phrase()))]], [sg.Text('Your incorrect guesses are: ' + ' '.join(self.graveyard)), sg.InputText()],
                 [sg.Button('Ok'), sg.Button('Cancel')] ]
 
     def open_window(self):
         # Create the Window
         window = sg.Window('Hang Man!', self.layout)
-        while True: #this is where the game happens
+        while not self.man.win(): #this is where the game happens
+            
+            print("\nYou have [WIP] chances!")
+            print("Phrase: ",self.man.get_phrase())
+            print('Your incorrect guesses are: ', self.graveyard)
             # Event Loop to process "events" and get the "values" of the inputs
             event, values = window.read()
             if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
                 break
-            print('You entered ', values[1])
-            if event == 'Ok':
-                graveyard += values[1]
-                self.layout[1][1] = sg.Text(graveyard)
-                window.Refresh()
+            self.graveyard += values[1]
+            self.layout[1][1] = sg.Text(self.graveyard)
+            window.layout(self.layout) 
+            # so pysg has a problem with "reusing layouts" meaning the line above will throw an error.
+            #gotta figure out a different way to update the window.
         window.close()
 
 if __name__ == "__main__":
