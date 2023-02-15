2a472dbb3b25d863f10bb7e7876c0bff97daa737
Tayo Olofintuyi <tolofi152@gmail.com>
2023-02-15 11:04:22 -0500

    didn't make much progress, still trying to figure out changing layouts

--- gui.py
+++ gui.py
@@ -8,7 +8,7 @@ class gui:
         self.pictures = ['./pic.png']
         self.man = HangMan()
         self.graveyard = self.man._graveyard
-        self.layout = [ [ [sg.Image(self.pictures[0])], [sg.Text("Phrase: " + ' '.join(self.man.get_phrase()))]], [sg.Text('Your incorrect guesses are: ' + ' '.join(self.graveyard)), sg.InputText()],
+        self.layout = [ [ [sg.Image(self.pictures[0])], [sg.Text("Phrase: " + ' '.join(self.man.get_phrase()))]], [sg.Text('Your incorrect guesses are: ' + ' '.join(self.graveyard) + "\n\n"), sg.InputText()],
                 [sg.Button('Ok'), sg.Button('Cancel')] ]
 
     def open_window(self):
@@ -24,10 +24,9 @@ class gui:
             if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
                 break
             self.graveyard += values[1]
-            self.layout[1][1] = sg.Text(self.graveyard)
-            window.layout(self.layout) 
-            # so pysg has a problem with "reusing layouts" meaning the line above will throw an error.
-            #gotta figure out a different way to update the window.
+            self.layout[1][0] = sg.Text('Your incorrect guesses are: ' + ' '.join(self.graveyard))
+            #window['grave'].update(self.graveyard) 
+            #gotta figure out a way to update the window.
         window.close()
 
 if __name__ == "__main__":
